b8eda46b0375fe81c96e35b7cd2077babeccd927
IIxauII <xauyxau@gmail.com>
2021-10-07 09:36:23 +0200

    update certs

--- shellscripts/updateCerts.sh
+++ shellscripts/updateCerts.sh
@@ -1,18 +1,21 @@
 #
 # updating certs with certbot
 #
-echo "Updating certs & restarting api server!";
+echo "Updating certs!";
 sudo certbot renew;
 # copy certs into project folder for usage in api server
+echo "Copying certs into project dir";
 CERTSDIR='/etc/letsencrypt/live/sose.bounceme.net';
-#sudo rsync —-copy-links $CERTSDIR/fullchain.pem $CERTSDIR/privkey.pem /home/sose/sose/certs/;
-sudo cp $CERTSDIR/fullchain.pem /home/sose/sose/certs/;
-sudo cp $CERTSDIR/privkey.pem /home/sose/sose/certs/;
+sudo cp -v $CERTSDIR/fullchain.pem /home/sose/sose/certs/;
+sudo cp -v $CERTSDIR/privkey.pem /home/sose/sose/certs/;
 # adjust permissions
+echo "Adjusting file permissions";
 sudo chmod -R ugo+r /home/sose/sose/certs;
 # kills forever running api.js
+echo "Stopping api server";
 forever stop api.js;
 # start forever api.js again with the new files
+echo "Starting api server!";
 cd /home/sose/sose/api/;
 forever start api.js;
-echo "Done updating certs & restarting api";
+echo "done";
