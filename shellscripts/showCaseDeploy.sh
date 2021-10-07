caf9c6e80035eb7840fb139f3a392d235f8fcf73
IIxauII <xauyxau@gmail.com>
2021-10-07 10:50:18 +0200

    add deployment script

---
+++ shellscripts/showCaseDeploy.sh
@@ -0,0 +1,20 @@
+
+#
+# Should help with deploying
+#
+echo "Alright lets deploy some SOSE!";
+echo "";
+echo "Fetching latest git changes!";
+cd /home/sose/sose/;
+git pull;
+echo "Removing currently running build!";
+cd /var/www/html/;
+rm -rf *;
+echo "Creating new showcase build!"
+cd /home/sose/sose/sodexosearcher/;
+npm install;
+npm run build:showcase;
+echo "Deploying new showcase build!";
+cp -R dist/* /var/www/html/;
+echo "sose deploy done!";
+#
