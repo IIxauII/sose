d62134dff56cef885af1d5248f6103ef4ebcb282
IIxauII <33882825+IIxauII@users.noreply.github.com>
2021-11-25 18:28:03 +0100

    introduce database restructuring

---
+++ saver/update-helpers/03-structure-cleanup/drop-table.js
@@ -0,0 +1,27 @@
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
+const serverConfig = require('../../config/saver-config.json');
+
+// TODO: include this in `setup-database` 
+//       OR 
+//       extend setup shellscript to execute this `extend-database.js` file afterwards
+//       OR
+//       create new shellscript for extending existing installations (e.g. like a patch framework)
+
+sqlite3.verbose();
+open({
+    filename: '../../' + serverConfig.db.path + serverConfig.db.name,
+    driver: sqlite3.Database
+}).then(async (db) => {
+    console.log('DB connection is up, ready for saving new data!');
+
+    // extends database table structure & adds here_id column
+    // cities
+    // name | post_code | sodexo_partners | here_id
+    await db.exec('DROP TABLE cities_new');
+    await db.exec('DROP TABLE partners');
+    console.log('Say bb to db!');
+    db.close();
+}).catch((err) => {
+    throw err;
+})
