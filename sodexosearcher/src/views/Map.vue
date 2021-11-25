48cffaa58e4d80d71e07108d5ef43ba2fb3da558
IIxauII <33882825+IIxauII@users.noreply.github.com>
2021-11-25 10:52:10 +0100

    updated POC - deactivated exposed api key - added config import

--- sodexosearcher/src/views/Map.vue
+++ sodexosearcher/src/views/Map.vue
@@ -1,5 +1,7 @@
 <template>
   <ion-page>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <link rel="stylesheet" type="text/css" href="https://js.api.here.com/v3/3.1/mapsjs-ui.css" />
     <default-header headerTitle="Map"></default-header>
     <ion-content fullscreen> </ion-content>
     <div
@@ -13,7 +15,8 @@
 import { IonPage, IonContent } from "@ionic/vue";
 import DefaultHeader from "../components/global/DefaultHeader.vue";
 import H from "@here/maps-api-for-javascript";
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions, mapMutations } from 'vuex';
+import hereConfig from '../../../configs/here.json';
 
 export default {
   name: "Map",
@@ -25,29 +28,50 @@ export default {
   data() {
     return {
       platform: null,
-      apikey: "p1Ftvj6JG46SrSX95XIzaOfc29pemVIADKd9h8YTksw", // DO NOT PUSH THIS
-      center: { lat: 40.73061, lng: -73.935242 },
+      apikey: hereConfig.apiKey,
+      center: { lat: 49.01094, lng: 8.40845 },
       map: null,
-      mapUi: H.ui.UI,
+      mapUi: null,
+      markerGroup: null,
     };
   },
   computed: {
     ...mapGetters('geo', {
       getLocation: 'getLocation',
     }),
-    ...mapGetters('citites', {
+    ...mapGetters('cities', {
       getCities: 'getCities',
     }),
     ...mapGetters('partners', {
-      // need to sort by nearest city of current view port and load partners accordingly
-    })
+      getPartnersOfCurrentCity: 'getPartnersOfCurrentCity',
+    }),
   },
   watch: {
     getLocation(newValue, oldValue) {
       console.log('getLocation', newValue);
       if (newValue.lat !== 0) {
-        this.center = newValue;
-        this.updateCenter();
+        setTimeout(() => {
+          this.center = newValue;
+          this.updateCenter();
+        }, 500)
+      }
+    },
+    getCities(newValue, oldValue) {
+      console.log('getCities', newValue);
+      if (newValue && newValue[0].distance) {
+        setTimeout(() => {
+          console.log('fetching Partners of City');
+          this.setCurrentCity(newValue[0].name);
+          this.fetchCityWithPartners(newValue[0].name);
+        }, 500);
+      }
+    },
+    getPartnersOfCurrentCity(newValue, oldValue) {
+      console.log('getPartnersOfCurrentCity', newValue);
+      if (newValue) {
+        setTimeout(() => {
+          this.addPartnersToMap(newValue);
+        }, 500);
       }
     }
   },
@@ -61,6 +85,12 @@ export default {
     }, 500);
   },
   methods: {
+    ...mapActions('partners', {
+      fetchCityWithPartners: 'fetchCityWithPartners',
+    }),
+    ...mapMutations('partners', {
+      setCurrentCity: "setCurrentCity",
+    }),
     initializeHereMap() {
       const mapContainer = this.$refs.hereMap;
       const mapTypes = this.platform.createDefaultLayers();
@@ -71,17 +101,51 @@ export default {
       });
 
       addEventListener("resize", () => {
+        console.log('resize triggered!');
         this.map.getViewPort().resize();
       });
 
       new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
       this.mapUi = H.ui.UI.createDefault(this.map, mapTypes);
-      //this.$forceUpdate();
     },
     updateCenter() {
       if (this.mapUi) {
         // man i really need to start using typescript..
-        this.mapUi.getMap().setCenter(this.center, true);
+        this.mapUi.getMap().setCenter(this.center, false);
+      }
+    },
+    addPartnersToMap(cityWithPartners) {
+      if (cityWithPartners) {
+        if (!this.group) {
+          this.group= new H.map.Group()
+          this.map.addObject(this.group);
+        }
+        cityWithPartners.sodexoPartners.forEach((partner) => {
+          const marker = new H.map.Marker({ lat: partner.lat, lng: partner.lng })
+          /* const domMarker = new H.map.DomMarker(
+            { lat: partner.lat, lng: partner.lng },
+            { data: partner.name },
+          ); */
+          marker.setData(`<p>${partner.name}</p>`);
+          marker.addEventListener('tap', (event) => {
+            const bubbles = this.mapUi.getBubbles();
+            if (bubbles && bubbles.length) {
+              console.log(bubbles.length);
+                bubbles.forEach((bubble) => {
+                  this.mapUi.removeBubble(bubble);
+              });
+            }
+            
+            const bubble = new H.ui.InfoBubble(
+                event.target.getGeometry(),
+                {
+                  content: event.target.getData(),
+                }
+            );
+            this.mapUi.addBubble(bubble);
+          }, false);
+          this.group.addObject(marker);
+        });
       }
     },
   },
