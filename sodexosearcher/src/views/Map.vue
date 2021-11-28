3f3bbf5e42f1065e0bfb761f3d6287c8a79a1ce9
IIxauII <33882825+IIxauII@users.noreply.github.com>
2021-11-28 18:46:33 +0100

    update map.vue to load near partners

--- sodexosearcher/src/views/Map.vue
+++ sodexosearcher/src/views/Map.vue
@@ -49,10 +49,6 @@ export default {
       if (newValue.lat !== 0) {
         setTimeout(() => {
           this.center = newValue;
-          this.fetchNearPartners({
-            ...newValue,
-            maxDistance: 1,
-          });
           this.updateCenter();
         }, 500)
       }
@@ -67,12 +63,6 @@ export default {
     }
   },
   mounted() {
-    if (this.getLocation) {
-      this.fetchNearPartners({
-        ...this.getLocation,
-        maxDistance: 5,
-      });
-    }
     setTimeout(() => {
       console.log('trying to add here map service now!');
       this.platform = new H.service.Platform({
@@ -90,17 +80,52 @@ export default {
       const mapTypes = this.platform.createDefaultLayers();
 
       this.map = new H.Map(mapContainer, mapTypes.vector.normal.map, {
-        zoom: 15,
+        zoom: 17,
         center: this.center,
       });
+      console.log('this.map', this.map);
 
       addEventListener("resize", () => {
         console.log('resize triggered!');
         this.map.getViewPort().resize();
       });
 
+      this.map.addEventListener('mapviewchangeend', () => {
+        console.log('mapviewchangeend triggered!');
+        const center = this.map.getCenter();
+        const zoomLevel = this.map.getZoom();
+        //const minZoomLevel = 2;
+        //const maxZoomlevel = 22;
+        let maxDistance = 0;
+        if (zoomLevel <= 8) {
+          maxDistance = 100;
+        } else if (zoomLevel <= 10) {
+          maxDistance = 50;
+        } else if (zoomLevel <= 12) {
+          maxDistance = 10
+        } else if(zoomLevel <= 14) {
+          maxDistance = 3;
+        } else if(zoomLevel <= 16) {
+          maxDistance = 2;
+        } else if (zoomLevel <= 18) {
+          maxDistance = 1;
+        }
+        console.log('center', center);
+        console.log('zoomLevel', zoomLevel);
+        console.log('maxDistance', maxDistance);
+        this.fetchNearPartners({
+          lat: center.lat,
+          lng: center.lng,
+          maxDistance,
+        });
+      });
+
       new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
       this.mapUi = H.ui.UI.createDefault(this.map, mapTypes);
+
+      if (this.getLocation) {
+        console.log(this.getLocation);
+      }
     },
     updateCenter() {
       if (this.mapUi) {
@@ -109,17 +134,15 @@ export default {
       }
     },
     addPartnersToMap(nearPartners) {
+      console.log('addingPartnersToMap');
       if (nearPartners) {
-        if (!this.group) {
-          this.group= new H.map.Group()
-          this.map.addObject(this.group);
-        }
+        if (this.group) {
+          this.map.removeObject(this.group);
+        } 
+        this.group= new H.map.Group();
+        this.map.addObject(this.group);
         nearPartners.forEach((partner) => {
-          const marker = new H.map.Marker({ lat: partner.lat, lng: partner.lng })
-          /* const domMarker = new H.map.DomMarker(
-            { lat: partner.lat, lng: partner.lng },
-            { data: partner.name },
-          ); */
+          const marker = new H.map.Marker({ lat: partner.lat, lng: partner.lng });
           marker.setData(`<p>${partner.name}</p>`);
           marker.addEventListener('tap', (event) => {
             const bubbles = this.mapUi.getBubbles();
